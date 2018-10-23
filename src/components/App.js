1b95018912d50e2a4973757c665db3dda08c3b24
jontorregano <jontorregano@gmail.com>
2018-10-23 12:27:08 -0500

    Initial Commit for testing work react-redux

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from  'react';
+import CommentList from "./CommentList";
+import CommentBox from "./CommentBx";
+
+export default () =>{
+    return(
+        <div>
+            <CommentBox/>
+            <CommentList/>
+        </div>
+    )
+};
